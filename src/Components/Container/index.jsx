762e22d82583a7ca9617bb25e078c57f517416d4
Georgi Santana <georgi.andersson@hotmail.com>
2022-10-19 23:52:22 -0300

    Refatorado os arquivos Container e Conteúdo, utilizando o styled-components

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,17 +1,27 @@
 import React from "react";
-
+import slyled from "styled-components";
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 
-const Container = () => {
+const Container = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+const Couteudo = styled.section`
+display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+`;
+
+export default () => {
   return (
-    <div className="container">
+    <Container>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <Couteudo>
         <Conta />
-      </section>
-    </div>
+      </Couteudo>
+    </Container>
   );
 };
-
-export default Container;
