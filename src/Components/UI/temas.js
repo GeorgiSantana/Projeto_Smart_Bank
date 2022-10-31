c90e0f76f167b3c1efcc0c67a8dc0ae74656a326
Georgi Santana <georgi.andersson@hotmail.com>
2022-10-30 23:59:23 -0300

    Criado componente Tema Escuro

---
+++ src/Components/UI/temas.js
@@ -0,0 +1,21 @@
+import { 
+    fundoClaro, 
+    conteudoClaro, 
+    textoEscuro, 
+    fundoEscuro, 
+    conteudoEscuro, 
+    textoFundoEscuro,
+    textoFundoClaro, 
+} from "./variaveis";
+
+export const temaClaro = {
+    body: fundoClaro,
+    inside: conteudoClaro,
+    text: textoFundoClaro,
+}
+
+export const temaEscuro = {
+    body: fundoEscuro,
+    inside: conteudoEscuro,
+    text: textoFundoEscuro,
+}
